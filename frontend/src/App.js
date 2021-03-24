3073848d37ad220e1622e090c51c5c2d19b3337a
Aupkist <justclam@protonmail.com>
2021-03-23 22:11:44 -0400

    Week3 HW1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,87 @@
 import logo from './CROWD.png';
 import './App.css';
+// Import FirebaseAuth and firebase.
+import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+ 
+// Configure Firebase.
+var firebaseConfig = {
+  apiKey: "AIzaSyBMcOoAyFEAcy5xN6H6iWVl-qA_7GTVJlY",
+  authDomain: "nanio-4c0b0.firebaseapp.com",
+  projectId: "nanio-4c0b0",
+  storageBucket: "nanio-4c0b0.appspot.com",
+  messagingSenderId: "1087403262812",
+  appId: "1:1087403262812:web:7d11778788457e5897b799"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+ 
+class SignInScreen extends React.Component {
+ 
+  // The component's Local state.
+  state = {
+    isSignedIn: false // Local signed-in state.
+  };
+ 
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: () => false
+    }
+  };
+ 
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+        (user) => this.setState({isSignedIn: !!user})
+    );
+  }
+  
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+ 
+  render() {
+    if (!this.state.isSignedIn) {
+      return (
+        <div>
+          <h1>Crowd</h1>
+          <p>Please sign-in:</p>
+          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <h1>Crowd</h1>
+        <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+        <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+      </div>
+    );
+  }
+}
+
+
+
+
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <SignInScreen />
         <p>
           About
         </p>
@@ -24,6 +100,8 @@ function App() {
       </header>
     </div>
   );
+
+
 }
 
 export default App;
